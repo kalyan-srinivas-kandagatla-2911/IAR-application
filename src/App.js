8367e56aac056aa711607a339711b320e802c63f
Kalyan Srinivas Kandagatla <kalyansrinivas2911@gmail.com>
2023-05-23 23:05:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="container">
+  <div class="box">Box 1</div>
+  <div class="box">Box 2</div>
+  <div class="box centered">Box 3</div>
+  <div class="box opacity">Box 4</div>
+  <div class="box opacity">Box 5</div>
+</div>
   );
 }
 
